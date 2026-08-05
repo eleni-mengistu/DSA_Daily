0d7969b99c69664f4cded8b5fd70a3a69c5ba1f9
Hprogramm <164498315+Hprogramm@users.noreply.github.com>
2026-08-06 00:12:01 +0300

    Remove Element using two pointers

--- arrays/remove_element.py
+++ arrays/remove_element.py
@@ -1,17 +1,10 @@
-# class Solution:
-#     def removeElement(self, nums: list[int], val: int) -> int:
-#         while val in nums:
-#             nums.remove(val)
-#         return len(nums)
-
-
 # Solution (Time Complexity: O(n), Space Complexity: O(1))
 # Uses two pointers to move all elements that are not equal to val
 # to the beginning of the array.
-
 # Note:
 # - The array is modified in place without using extra space.
 # - k keeps track of the position where the next valid element is placed.
+
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
         k = 0
